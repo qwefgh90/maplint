0389ff3eee556cff1bd4c86bfc374fb6f3212123
Choe Changwon <qwefgh90@naver.com>
2022-11-25 18:31:29 +0900

    changes for processing maven better

--- java-eager/src/main/java/els/project/MavenProject.java
+++ java-eager/src/main/java/els/project/MavenProject.java
@@ -13,11 +13,6 @@ import java.util.stream.Collectors;
 public class MavenProject extends JavaProject {
     protected final Path pomFile;
     protected MavenProjectUpdater updater;
-    /*public MavenProject(Path root, Set<Artifact> dependencies, Set<Path> docs, Set<Path> classPath, Set<String> exports, Set<String> externalDependencies, Path pomFile, MavenProjectUpdater updater) throws JavaProjectInitializationError {
-        super(root, dependencies.stream().map(d -> d.getFile().toPath()).collect(Collectors.toSet()), docs, classPath, exports, externalDependencies);
-        this.pomFile = pomFile;
-        this.updater = updater;
-    }*/
     public MavenProject(Path root, Set<Path> dependencies, Set<Path> docs, Set<Path> classPath, Set<String> exports, Set<String> externalDependencies, Path pomFile, MavenProjectUpdater updater) throws JavaProjectInitializationError {
         super(root, dependencies, docs, classPath, exports, externalDependencies);
         this.pomFile = pomFile;
--- java-eager/src/main/java/els/project/MavenProjectFactory.java
+++ java-eager/src/main/java/els/project/MavenProjectFactory.java
@@ -33,15 +33,22 @@ public class MavenProjectFactory extends JavaProjectFactory implements MavenProj
 
     @Override
     public JavaProject create(Set<String> externalDependencies, Set<Path> classPath, Set<String> exports) throws JavaProjectInitializationError {
-        //var dependencies = mvnDependencies(pomXml, "dependency:list");
-        Set<Path> dependencies = null;
         try {
-            dependencies = MavenUtils.dependencies(workspace, projectRoot).stream().map(d -> d.getFile().toPath()).collect(Collectors.toSet());
+            var dependencies = MavenUtils.dependencies(projectRoot);
+            var docs = Collections.EMPTY_SET;
+//            var dependencies = mvnDependencies(pomXml, "dependency:list");
+            return new MavenProject(projectRoot, dependencies.stream().map(d -> d.getFile().toPath()).collect(Collectors.toSet()), docs, classPath, exports, externalDependencies, pomXml, this);
         } catch (ModelBuildingException e) {
             throw new RuntimeException(e);
         }
-        var docs = Collections.EMPTY_SET;
-        return new MavenProject(projectRoot, dependencies, docs, classPath, exports, externalDependencies, pomXml, this);
+    }
+
+    @Override
+    public void updateDependencies(MavenProject project) throws ModelBuildingException {
+//        var dependencies = mvnDependencies(pomXml, "dependency:list");
+//        var docs = mvnDependencies(pomXml, "dependency:sources");
+//        project.docs = docs;
+        project.dependencies = MavenUtils.dependencies(projectRoot).stream().map(d -> d.getFile().toPath()).collect(Collectors.toSet());
     }
 
     /**
@@ -50,95 +57,86 @@ public class MavenProjectFactory extends JavaProjectFactory implements MavenProj
      * @param goal
      * @return
      */
-    protected Set<Path> mvnDependencies(Path pomXml, String goal) {
-        assert Files.exists(pomXml);
-        try {
-            // TODO consider using mvn valide dependency:copy-dependencies -DoutputDirectory=??? instead
-            // Run maven as a subprocess
-            String[] command = {
-                    getMvnCommand(),
-                    "--batch-mode", // Turns off ANSI control sequences
-                    "validate",
-                    goal,
-                    "-DincludeScope=test",
-                    "-DoutputAbsoluteArtifactFilename=true",
-            };
-            var output = Files.createTempFile("java-language-server-maven-output", ".txt");
-            var errorOutput = Files.createTempFile("java-language-server-maven-error-output", ".txt");
-            logger.debug("Execute {}", String.join(" ", command));
-            logger.debug("Reading dependencies from {} ({})", pomXml.toAbsolutePath().toString(), String.join(" ", command));
-            var workingDirectory = pomXml.toAbsolutePath().getParent().toFile();
-            var process =
-                    new ProcessBuilder()
-                            .command(command)
-                            .directory(workingDirectory)
-                            .redirectError(ProcessBuilder.Redirect.INHERIT)
-                            .redirectOutput(output.toFile())
-                            .start();
-            // Wait for process to exit
-            var result = process.waitFor();
+//    protected Set<Path> mvnDependencies(Path pomXml, String goal) {
+//        assert Files.exists(pomXml);
+//        try {
+//            // Run maven as a subprocess
+//            String[] command = {
+//                    getMvnCommand(),
+//                    "--batch-mode", // Turns off ANSI control sequences
+//                    "validate",
+//                    goal,
+//                    "-DincludeScope=test",
+//                    "-DoutputAbsoluteArtifactFilename=true",
+//            };
+//            var output = Files.createTempFile("java-language-server-maven-output", ".txt");
+//            var errorOutput = Files.createTempFile("java-language-server-maven-error-output", ".txt");
+//            logger.debug("Execute {}", String.join(" ", command));
+//            logger.debug("Reading dependencies from {} ({})", pomXml.toAbsolutePath().toString(), String.join(" ", command));
+//            var workingDirectory = pomXml.toAbsolutePath().getParent().toFile();
+//            var process =
+//                    new ProcessBuilder()
+//                            .command(command)
+//                            .directory(workingDirectory)
+//                            .redirectError(ProcessBuilder.Redirect.INHERIT)
+//                            .redirectOutput(output.toFile())
+//                            .start();
+//            // Wait for process to exit
+//            var result = process.waitFor();
+//
+//            if (result != 0) {
+//                logger.warn("the exit value is {}.\n the error is from {}.\n {}", result, output.toAbsolutePath(), Files.readString(output));
+//                return Set.of();
+//            }
+//            // Read output
+//            var dependencies = new HashSet<Path>();
+//            logger.debug("The dependency list is following.");
+//            for (var line : Files.readAllLines(output)) {
+//                var jar = readDependency(line);
+//                if (jar != NOT_FOUND) {
+//                    dependencies.add(jar);
+//                }
+//            }
+//            return dependencies;
+//        } catch (InterruptedException | IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
-            if (result != 0) {
-                logger.warn("the exit value is {}.\n the error is from {}.\n {}", result, output.toAbsolutePath(), Files.readString(output));
-                return Set.of();
-            }
-            // Read output
-            var dependencies = new HashSet<Path>();
-            logger.debug("The dependency list is following.");
-            for (var line : Files.readAllLines(output)) {
-                var jar = readDependency(line);
-                if (jar != NOT_FOUND) {
-                    dependencies.add(jar);
-                }
-            }
-            return dependencies;
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    protected final Pattern DEPENDENCY_PATTERN =
+//            Pattern.compile("^\\[INFO\\]\\s+(.*:.*:.*:.*:.*):((/|\\w:\\\\).*?)( -- module .*)?$");
+//
+//    protected final Path NOT_FOUND = Paths.get("");
+//
+//    protected Path readDependency(String line) {
+//        var match = DEPENDENCY_PATTERN.matcher(line);
+//        if (!match.matches()) {
+//            return NOT_FOUND;
+//        }
+//        var artifact = match.group(1);
+//        var path = match.group(2);
+//        logger.debug(String.format("=> %s (%s)", artifact, path));
+//        return Paths.get(path);
+//    }
 
-    protected final Pattern DEPENDENCY_PATTERN =
-            Pattern.compile("^\\[INFO\\]\\s+(.*:.*:.*:.*:.*):((/|\\w:\\\\).*?)( -- module .*)?$");
+//    protected String getMvnCommand() {
+//        var mvnCommand = "mvn";
+//        if (File.separatorChar == '\\') {
+//            mvnCommand = findExecutableOnPath("mvn.cmd");
+//            if (mvnCommand == null) {
+//                mvnCommand = findExecutableOnPath("mvn.bat");
+//            }
+//        }
+//        return mvnCommand;
+//    }
 
-    protected final Path NOT_FOUND = Paths.get("");
-
-    protected Path readDependency(String line) {
-        var match = DEPENDENCY_PATTERN.matcher(line);
-        if (!match.matches()) {
-            return NOT_FOUND;
-        }
-        var artifact = match.group(1);
-        var path = match.group(2);
-        logger.debug(String.format("=> %s (%s)", artifact, path));
-        return Paths.get(path);
-    }
-
-    protected String getMvnCommand() {
-        var mvnCommand = "mvn";
-        if (File.separatorChar == '\\') {
-            mvnCommand = findExecutableOnPath("mvn.cmd");
-            if (mvnCommand == null) {
-                mvnCommand = findExecutableOnPath("mvn.bat");
-            }
-        }
-        return mvnCommand;
-    }
-
-    protected String findExecutableOnPath(String name) {
-        for (var dirname : System.getenv("PATH").split(File.pathSeparator)) {
-            var file = new File(dirname, name);
-            if (file.isFile() && file.canExecute()) {
-                return file.getAbsolutePath();
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public void updateDependencies(MavenProject project) {
-        var dependencies = mvnDependencies(pomXml, "dependency:list");
-        var docs = mvnDependencies(pomXml, "dependency:sources");
-        project.dependencies = dependencies;
-        project.docs = docs;
-    }
+//    protected String findExecutableOnPath(String name) {
+//        for (var dirname : System.getenv("PATH").split(File.pathSeparator)) {
+//            var file = new File(dirname, name);
+//            if (file.isFile() && file.canExecute()) {
+//                return file.getAbsolutePath();
+//            }
+//        }
+//        return null;
+//    }
 }
